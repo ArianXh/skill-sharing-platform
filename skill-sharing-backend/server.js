dce15d8c40fe348af9982fd02a52161d4ebe2975
unknown <arianxhumkar@hotmail.com>
2024-08-19 14:52:27 +0200

    Created a connection to PostreSQL database locally, created a models folder with Users model and added a POST request to confirm the connection to the database, everything works properly

--- skill-sharing-backend/server.js
+++ skill-sharing-backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const sequelize = require('./config/database');
+const User = require('.//../models/User');
 require('dotenv').config();
 
 const app = express();
@@ -12,6 +14,31 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Skill-Sharing Backend API');
 });
+
+
+// Sync Sequelize with the database
+sequelize.sync()
+.then(() => {
+  console.log('PostgreSQL connected and synced');
+})
+.catch(err => {
+  console.error('PostgreSQL connection error:', err);
+});
+
+
+// Route to create a new user (for verification)
+app.post('/users', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const newUser = await User.create({ name, email, password });
+        res.status(201).json(newUser);
+    } catch(error){
+        console.error(error);
+        res.status(500).json({ error: 'Error creating user' });
+    }
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
